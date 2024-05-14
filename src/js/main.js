544447082b216ab53ca5aa38f3eb25339bfe35cd
TalehHesenov <talehhesenov19944@gmail.com>
2024-05-14 13:49:24 +0400

    new edit

--- src/js/main.js
+++ src/js/main.js
@@ -8,13 +8,12 @@ let en =Number(document.getElementById("en").value);
     
 
     document.getElementById("netice").innerHTML= cavab;
-
     
 }
 function sahesi(){
     let uzunluq = Number(document.getElementById("uzunluq").value);
 let en =Number(document.getElementById("en").value);
-    let sahe = uzunluq+en;
+    let sahe = uzunluq*en;
     
 
     document.getElementById("netice").innerHTML= sahe;
